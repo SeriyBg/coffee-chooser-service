82bdf88595359776d4cd91aca0167c05ccb2bdd2
Serge Bishyr <seriybg@gmail.com>
2019-08-26 22:08:55 +0300

    random error

--- src/main/kotlin/com/coffee/chooser/coffeeservice/CoffeeController.kt
+++ src/main/kotlin/com/coffee/chooser/coffeeservice/CoffeeController.kt
@@ -2,13 +2,16 @@ package com.coffee.chooser.coffeeservice
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import kotlin.random.Random
 
 @RestController
 class CoffeeController(val coffeeConfigProperties: CoffeeConfigProperties) {
 
     @GetMapping("/coffee")
     fun chooseCoffee() : Coffee? {
-        throw IllegalArgumentException("Unexpected bug occurred!")
+        if (Random.nextBoolean()) {
+            throw IllegalArgumentException("Unexpected bug occurred!")
+        }
         return coffeeConfigProperties.coffee?.shuffled()?.get(0)
     }
 }
