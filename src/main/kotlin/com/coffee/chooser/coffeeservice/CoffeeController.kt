b2ccef114243852cf04236e87bea3321290ee556
Serge Bishyr <seriybg@gmail.com>
2019-09-07 12:53:40 +0300

    chage error code

--- src/main/kotlin/com/coffee/chooser/coffeeservice/CoffeeController.kt
+++ src/main/kotlin/com/coffee/chooser/coffeeservice/CoffeeController.kt
@@ -1,6 +1,8 @@
 package com.coffee.chooser.coffeeservice
 
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -8,7 +10,10 @@ class CoffeeController(val coffeeConfigProperties: CoffeeConfigProperties) {
 
     @GetMapping("/coffee")
     fun chooseCoffee() : Coffee? {
-        throw IllegalArgumentException("Unexpected bug occurred!")
+        throw BadGateway()
         return coffeeConfigProperties.coffee?.shuffled()?.get(0)
     }
 }
+
+@ResponseStatus(value = HttpStatus.BAD_GATEWAY)
+class BadGateway : RuntimeException()
